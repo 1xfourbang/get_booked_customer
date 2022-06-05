288c311e274a1a87d7a787b087d6d4b1b6124a18
1xfour_bang <13113413+1xfourbang@users.noreply.github.com>
2022-06-05 13:08:27 -0600

    homepage mobile responsive

--- src/app/layouts/HomeLayouts.jsx
+++ src/app/layouts/HomeLayouts.jsx
@@ -4,13 +4,22 @@ import logo_mobile from '../../assets/images/logo_mobile.png'
 
 const HomeHeader = () => {
   return (
-    <div className="fixed flex items-center justify-between w-full px-12 h-30">
-      <div className="self-end logo-wrapper">
+    <div className="flex items-center justify-center w-full px-12 sm:justify-between sm:fixed h-14 sm:h-30">
+      <div className="lg:self-end logo-wrapper">
         <a href="/">
-          <img src={logo} alt="Desktop Logo" className="max-w-[19.5rem]" />
+          <img
+            src={logo}
+            alt="Desktop Logo"
+            className="max-w-[14rem] lg:max-w-[19.5rem] hidden sm:block"
+          />
+          <img
+            src={logo_mobile}
+            alt="Mobile Logo"
+            className="max-w-[8.375rem] block sm:hidden"
+          />
         </a>
       </div>
-      <div className="signup-wrapper">
+      <div className="hidden signup-wrapper sm:block">
         <a
           href="/signup"
           className="font-avenir-bold text-[15px] leading-5 text-black px-7 h-13 flex items-center rounded-md bg-white"
@@ -24,8 +33,8 @@ const HomeHeader = () => {
 
 const HomeFooter = () => {
   return (
-    <div className="flex items-center justify-center h-[150px] bg-grey">
-      <p className="font-avenir-medium text-5">
+    <div className="flex items-center justify-center h-18 sm:h-[150px] bg-grey">
+      <p className="font-avenir-medium text-3 sm:text-5">
         Copyright 2022 Vitalize Business Solutions www.vitalize.ie
       </p>
     </div>
