6f08449884dbce691dadd08e0aefbabde362e7f2
Oleg Kovelsky <oleg.kovelsky.job@gmail.com>
2022-06-05 13:08:27 -0600

    homepage mobile responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
       },
       backgroundImage: {
         'home-hero': "url('./assets/images/foreground.png')",
+        'home-hero-mobile': "url('./assets/images/foreground_mobile.png')",
       },
       height: {
         18: '4.5rem',
@@ -25,10 +26,14 @@ module.exports = {
         13: '3.25rem',
       },
       fontSize: {
+        3: ['.75rem', '16px'],
+        4.5: ['1.125rem', '25px'],
         5: ['1.25rem', '27px'],
         6.25: ['1.5625rem', '34px'],
         6.5: ['1.625rem', '36px'],
         6.75: ['1.6875rem', '37px'],
+        7: ['1.75rem', '28px'],
+        24.25: ['6.0625rem', '99px'],
       },
       colors: {
         grey: '#E8E8E8',
